117a7f7880df5fba50dd8f3eb4877285a4f5d2f5
David Divilly <d.divilly@gmail.com>
2018-01-21 18:11:07 +0000

    Added user_agent

--- lib/webscraper_mp.rb
+++ lib/webscraper_mp.rb
@@ -2,12 +2,16 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-# multipage webscrapper
+# so the site thinks we're browsing, rather than a program
+# browser user string, taken from:
+# http://www.whatismyuseragent.net/
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
 
+# multipage webscrapper
 url = "http://thechive.com/page/1"
 
 # nokogiri parses HTML and XML
-page = Nokogiri::HTML(open(url))
+page = Nokogiri::HTML(open(url, 'User-Agent' => user_agent))
 
 title_css = "h3.post-title.entry-title.card-title"
 likes_css = "span.card-count.icon-thumbsup.upvotes-count"
@@ -22,18 +26,19 @@ pages_css_aref = "nav.page-navi a"
 # end
 
 titles = []
-
 likes = []
-
 
 pages = []
 page.css(pages_css_aref).select do |page|
   pages << page.text.scan(/[0-9]/)
 end
 
+# not the last page number, only the last shown as an option on the website
+# there are 1000+ pages
 last_number = pages.max.join.to_i
 
 # incrementing/loop pages
+# going through each page, adding them to the array
 
 last_number.times do |i|
 
@@ -50,7 +55,7 @@ last_number.times do |i|
 
 end
 
-#
+# savind in the reports direcory
 CSV.open("./reports/theChive.csv", "w") do |file|
   file << ["Title", "Likes"]
 
