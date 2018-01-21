452844359f10709debcea05c7da562449634172f
David Divilly <d.divilly@gmail.com>
2018-01-21 18:30:21 +0000

    Added webscaper for forms

---
+++ lib/webscraper_google.rb
@@ -0,0 +1,20 @@
+require 'mechanize'
+
+agent = Mechanize.new
+
+page = agent.get('http://www.google.com')
+
+# form name
+google_form = page.form('f')
+
+# q is the name of the input
+google_form.q = 'udemy'
+
+page = agent.submit(google_form)
+
+# when searched in google
+udemy_link = 'h3.r'
+
+page.search(udemy_link).each do |link|
+  puts link.content
+end
