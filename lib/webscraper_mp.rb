afd8128af856ee7f01f8afd7a8772070a19f5143
David Divilly <d.divilly@gmail.com>
2018-01-21 15:23:28 +0000

    Added times for multipage

--- lib/webscraper_mp.rb
+++ lib/webscraper_mp.rb
@@ -4,7 +4,7 @@ require 'csv'
 
 # multipage webscrapper
 
-url = "http://thechive.com/"
+url = "http://thechive.com/page/1"
 
 # nokogiri parses HTML and XML
 page = Nokogiri::HTML(open(url))
@@ -13,26 +13,43 @@ title_css = "h3.post-title.entry-title.card-title"
 likes_css = "span.card-count.icon-thumbsup.upvotes-count"
 
 pages_css = "nav.page-navi"
+pages_css_aref = "nav.page-navi a"
 
 # finding the max number shown on the page. Note, that on theChive, it doesn't
 # show the full number of pages
-page.css(pages_css).each do |pages|
-  puts pages.text.scan(/[0-9]/).max
-end
+# page.css(pages_css).each do |pages|
+#   puts pages.text.scan(/[0-9]/).max
+# end
 
 titles = []
-page.css(title_css).each do |title|
-  titles << title.text
-end
 
 likes = []
-page.css(likes_css).each do |like|
-  likes << like.text
+
+
+pages = []
+page.css(pages_css_aref).select do |page|
+  pages << page.text.scan(/[0-9]/)
 end
-#
-# pages = []
-# page.css()
-#
+
+last_number = pages.max.join.to_i
+
+# incrementing/loop pages
+
+last_number.times do |i|
+
+  url = "http://thechive.com/page/#{i + 1}"
+  page = Nokogiri::HTML(open(url))
+
+  page.css(title_css).each do |title|
+    titles << title.text
+  end
+
+  page.css(likes_css).each do |like|
+    likes << like.text
+  end
+
+end
+
 #
 CSV.open("./reports/theChive.csv", "w") do |file|
   file << ["Title", "Likes"]
