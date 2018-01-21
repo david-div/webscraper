af3d3e63d74ee58b7f11460a395e22ff6f222497
David Divilly <d.divilly@gmail.com>
2018-01-21 14:51:13 +0000

    Added reports dir

--- lib/webscraper_mp.rb
+++ lib/webscraper_mp.rb
@@ -14,28 +14,30 @@ likes_css = "span.card-count.icon-thumbsup.upvotes-count"
 
 pages_css = "nav.page-navi"
 
+# finding the max number shown on the page. Note, that on theChive, it doesn't
+# show the full number of pages
 page.css(pages_css).each do |pages|
   puts pages.text.scan(/[0-9]/).max
 end
 
-# titles = []
-# page.css(title_css).each do |title|
-#   titles << title.text
-# end
-#
-# likes = []
-# page.css(likes_css).each do |like|
-#   likes << like.text
-# end
+titles = []
+page.css(title_css).each do |title|
+  titles << title.text
+end
+
+likes = []
+page.css(likes_css).each do |like|
+  likes << like.text
+end
 #
 # pages = []
 # page.css()
 #
 #
-# CSV.open("theChive.csv", "w") do |file|
-#   file << ["Title", "Likes"]
-#
-#   titles.length.times do |i|
-#     file << [titles[i], likes[i]]
-#   end
-# end
+CSV.open("./reports/theChive.csv", "w") do |file|
+  file << ["Title", "Likes"]
+
+  titles.length.times do |i|
+    file << [titles[i], likes[i]]
+  end
+end
